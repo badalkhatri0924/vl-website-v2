2e1a68904cbffac525299b813a7b1bda2d557767
Chirag Prajapati <chotajd4444@gmail.com>
2026-01-05 17:21:27 +0530

    added: sanity setup for blogs content

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,8 @@
 import BlogView from '@/components/BlogView'
+import { getBlogPosts } from '@/lib/sanity/utils'
 
-export default function BlogPage() {
-  return <BlogView />
+export default async function BlogPage() {
+  const posts = await getBlogPosts()
+  return <BlogView posts={posts} />
 }
 
